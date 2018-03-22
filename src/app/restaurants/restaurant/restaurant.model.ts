ed56c500b9b10eff1dae5eecb61a9ad7b3b246e7
fernando <fleis.wa@raiadrogasil.com.br>
2018-03-22 18:23:13 -0300

    Implementando as Rotas

---
+++ src/app/restaurants/restaurant/restaurant.model.ts
@@ -0,0 +1,12 @@
+export interface Restaurant {
+
+    id: String
+    name: String
+    category: String
+    deliveryEstimate: String
+    rating: number
+    imagePath: String
+
+    // "about": "A Bread & Brakery tem 40 anos de mercado. Fazemos os melhores doces e pães. Compre e confira.",
+    // "hours": "Funciona de segunda à sexta, de 8h às 23h"
+}
