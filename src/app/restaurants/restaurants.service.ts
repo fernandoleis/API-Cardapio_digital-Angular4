8c70c0cbb8aecfcd40198bdba325f7bb26f67b23
fernando <fleis.wa@raiadrogasil.com.br>
2018-03-24 11:59:47 -0300

    Usando o pipe async ao invés do subscribe - criando o componente de review

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -23,4 +23,10 @@ export class RestaurantsService {
       .map(response => response.json())
       .catch(ErrorHandler.handleError);
     }
+
+    reviewsOfRestaurant(id:string):Observable<any>{
+      return this.http.get(`${MEAT_API}/restarants/${id}/reviews`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError);
+    }
 }
