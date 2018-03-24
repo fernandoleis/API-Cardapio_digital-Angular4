54a722fd4e00bef59266fdc5f97bb9c30311e9a4
fernando <fleis.wa@raiadrogasil.com.br>
2018-03-24 10:38:48 -0300

    servico para popular os detalhes dos restaturantes

--- src/app/restaurants/restaurant/restaurant.model.ts
+++ src/app/restaurants/restaurant/restaurant.model.ts
@@ -1,12 +1,11 @@
 export interface Restaurant {
 
-    id: String
-    name: String
-    category: String
-    deliveryEstimate: String
-    rating: number
-    imagePath: String
-
-    // "about": "A Bread & Brakery tem 40 anos de mercado. Fazemos os melhores doces e pães. Compre e confira.",
-    // "hours": "Funciona de segunda à sexta, de 8h às 23h"
+    id: String;
+    name: String;
+    category: String;
+    deliveryEstimate: String;
+    rating: number;
+    imagePath: String;
+    about: String;
+    hours: String;
 }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -16,6 +16,11 @@ export class RestaurantsService {
         return this.http.get(`${MEAT_API}/restaurants`)
         .map(response => response.json())
         .catch(ErrorHandler.handleError);
+    }
 
+    restaurantByID(id:string):Observable<Restaurant>{
+      return this.http.get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError);
     }
 }
