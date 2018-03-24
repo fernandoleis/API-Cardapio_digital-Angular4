95939309028575096ec85f9e31f2dc10eefad9e1
fernando <fleis.wa@raiadrogasil.com.br>
2018-03-24 08:52:44 -0300

    Tratamentos de erro com operador catch

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from "@angular/core"
-import { Http, Response } from "@angular/http"
-import { Restaurant } from "./restaurant/restaurant.model";
-import { MEAT_API } from "../app.api"
-import { Observable } from "rxjs/Observable";
-import "rxjs/add/operator/map"
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Restaurant } from './restaurant/restaurant.model';
+import { MEAT_API } from '../app.api';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import {ErrorHandler} from '../app.error-handler';
 
 @Injectable()
 export class RestaurantsService {
@@ -11,6 +13,9 @@ export class RestaurantsService {
     constructor(private http: Http) { }
 
     restaurants(): Observable<Restaurant[]> {
-        return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json())
+        return this.http.get(`${MEAT_API}/restaurants`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError);
+
     }
 }
