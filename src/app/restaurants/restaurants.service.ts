3ff8d11ab679e13b6027ae2bf6a256e2b6a7da94
Fernando Leis <fernandoleis@gmail.com>
2018-05-23 21:12:36 -0300

    criação do menu item dinâmico

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -5,28 +5,35 @@ import { MEAT_API } from '../app.api';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import {ErrorHandler} from '../app.error-handler';
+import { ErrorHandler } from '../app.error-handler';
+import { MenuItem } from '../restaurant-detail/menu-item/menu-item.model';
 
 @Injectable()
 export class RestaurantsService {
 
-    constructor(private http: Http) { }
+  constructor(private http: Http) { }
 
-    restaurants(): Observable<Restaurant[]> {
-        return this.http.get(`${MEAT_API}/restaurants`)
-        .map(response => response.json())
-        .catch(ErrorHandler.handleError);
-    }
-
-    restaurantByID(id:string):Observable<Restaurant>{
-      return this.http.get(`${MEAT_API}/restaurants/${id}`)
+  restaurants(): Observable<Restaurant[]> {
+    return this.http.get(`${MEAT_API}/restaurants`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError);
-    }
+  }
 
-    reviewsOfRestaurant(id:string):Observable<any>{
-      return this.http.get(`${MEAT_API}/restarants/${id}/reviews`)
+  restaurantByID(id: string): Observable<Restaurant> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError);
-    }
+  }
+
+  reviewsOfRestaurant(id: string): Observable<any> {
+    return this.http.get(`${MEAT_API}/restarants/${id}/reviews`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError);
+  }
+
+  menuOfRestaurant(id: string): Observable<MenuItem[]> {
+    return this.http.get(`${MEAT_API}/restarants/${id}/menu`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError);
+  }
 }
