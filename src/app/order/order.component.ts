c05cc685e4d2f72fbc4fa5e29c4c98519199f4f6
Fernando Leis <fernandoleis@gmail.com>
2018-06-04 22:30:03 -0300

    Finalizando a compra - HTTP POST (68)

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RadioOption } from '../shared/radio/radio-option.model';
 import { OrderService } from './order.service';
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
+import { Order, OrderItem } from './order.model';
 
 @Component({
   selector: 'mt-order',
@@ -21,7 +22,7 @@ export class OrderComponent implements OnInit {
   ngOnInit() {
   }
 
-  itemsValue():number{
+  itemsValue(): number {
     return this.orderService.itemsValue();
   }
 
@@ -39,5 +40,14 @@ export class OrderComponent implements OnInit {
 
   remove(item: CartItem) {
     this.orderService.remove(item);
+  }
+
+  checkOrder(order: Order) {
+    order.orderItems = this.cartItems().map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
+    console.log(order);
+    this.orderService.checkOrder(order).subscribe((orderId: string) =>{
+      console.log(`Compra concluída: ${orderId}`);
+      this.orderService.clear();
+    } );
   }
 }
