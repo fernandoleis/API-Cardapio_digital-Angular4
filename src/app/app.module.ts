a69a0968ad077955e2e9bf5dcd6e671e6fb2ac10
Fernando Leis <fernandoleis@gmail.com>
2018-07-02 20:20:01 -0300

    adicionando serviços a um modulo compartilhado (80)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,6 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 import{SharedModule} from './shared/shared.module';
-import { CoreModule } from 'core/core.modelue';
 
 
 @NgModule({
@@ -38,8 +37,7 @@ import { CoreModule } from 'core/core.modelue';
   imports: [
     BrowserModule,
     HttpModule,
-    SharedModule,
-    CoreModule,
+    SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES)
   ],
   providers: [ { provide: LOCALE_ID, useValue: 'pt-BR' }],
