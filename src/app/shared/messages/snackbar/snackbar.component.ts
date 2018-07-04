25344d67bcacf3a10f4b980e4b5084137985b4fe
Fernando Leis <fernandoleis@gmail.com>
2018-07-03 21:45:54 -0300

    usando operadores do e switchMap (87)

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -3,6 +3,8 @@ import { trigger, transition, style, state, animate } from '@angular/animations'
 import { NotificationService } from '../notification.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'mt-snackbar',
@@ -30,11 +32,10 @@ export class SnackbarComponent implements OnInit {
   constructor(private notificationService: NotificationService) { }
 
   ngOnInit() {
-    this.notificationService.notifier.subscribe(message => {
+    this.notificationService.notifier.do(message => {
       this.message = message;
       this.snackVisibility = 'visible';
-      Observable.timer(3000).subscribe(timer => this.snackVisibility = 'hidden');
-    })
+    }).switchMap(message => Observable.timer(3000)).subscribe(timer => this.snackVisibility = 'hidden');
   }
 
 }
