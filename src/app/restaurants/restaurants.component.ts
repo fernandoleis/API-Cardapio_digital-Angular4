13583581f056356f4676dc5a56d1c745c9ecc102
Fernando Leis <fernandoleis@gmail.com>
2018-07-17 23:42:06 -0300

    prevenindo a interrupcao do Observable ValueChanges(106)

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -7,6 +7,9 @@ import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/from';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'mt-restaurants',
@@ -42,7 +45,15 @@ export class RestaurantsComponent implements OnInit {
       searchControl: this.searchControl
     });
 
-    this.searchControl.valueChanges.debounceTime(500).distinctUntilChanged().do(searchTerm => console.log(`q=${searchTerm}`)).switchMap(searchTerm => this.restaurantsService.restaurants(searchTerm)).subscribe(restaurants => this.restaurants = restaurants);
+    this.searchControl.valueChanges
+      .debounceTime(500)
+      .distinctUntilChanged()
+      .do(searchTerm => console.log(`q=${searchTerm}`))
+      .switchMap(searchTerm => this.restaurantsService
+        .restaurants(searchTerm)
+        .catch(error => Observable.from([])))
+      .subscribe(restaurants => this.restaurants = restaurants);
+
     this.restaurantsService.restaurants().subscribe(restaurants => this.restaurants = restaurants);
   }
 
