2778b7dc96c6fd5881ebc848fa2eeb84c889fb72
Fernando Leis <fernandoleis@gmail.com>
2018-08-09 23:16:54 -0300

    implementando a rota de login (111)

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,9 @@
 import * as jsonServer from 'json-server';
-import {Express} from 'express';
+import { Express } from 'express';
 import * as fs from 'fs';
 import * as https from 'https';
+import { handleAuthentication } from './auth';
+
 
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -13,6 +15,11 @@ server.use(middlewares)
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
+
+// Middleware  para login
+server.post('/login', handleAuthentication);
+
+server.use(router);
 
 const options = {
     cert: fs.readFileSync('./backend/keys/cert.pem'),
