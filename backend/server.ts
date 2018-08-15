5d46b63f7d78f2516ea4747486968e9c91f8bde7
Fernando Leis <fernandoleis@gmail.com>
2018-08-14 23:34:58 -0300

    Middleware de autorizacao (115)

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,7 @@ import { Express } from 'express';
 import * as fs from 'fs';
 import * as https from 'https';
 import { handleAuthentication } from './auth';
+import { handleAuthorization } from './authz';
 
 
 const server = jsonServer.create()
@@ -18,6 +19,7 @@ server.use(jsonServer.bodyParser)
 
 // Middleware  para login
 server.post('/login', handleAuthentication);
+server.use('/orders', handleAuthorization);
 
 server.use(router);
 
