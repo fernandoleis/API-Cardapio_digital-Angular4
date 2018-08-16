96b7d6fe4f40ca24b9ff3add6e1f4c2dd1525082
Fernando Leis <fernandoleis@gmail.com>
2018-08-15 23:25:53 -0300

    Criando o serviçod e Login (118)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,6 +9,7 @@ import { RestaurantsService } from '../restaurants/restaurants.service';
 import { OrderService } from '../order/order.service';
 import { SnackbarComponent } from './messages/snackbar/snackbar.component';
 import { NotificationService } from './messages/notification.service';
+import { LoginService } from '../security/login/login.service';
 
 @NgModule({
   declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
@@ -25,6 +26,6 @@ import { NotificationService } from './messages/notification.service';
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
-    return { ngModule: SharedModule, providers: [ShoppingCartService, RestaurantsService, OrderService, NotificationService] }
+    return { ngModule: SharedModule, providers: [ShoppingCartService, RestaurantsService, OrderService, NotificationService, LoginService] }
   }
 }
