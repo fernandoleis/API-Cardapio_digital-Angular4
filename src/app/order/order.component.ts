994c80b615b0789a75158c360dcc1aeee5c66cbd
Fernando Leis <fernandoleis@gmail.com>
2018-08-21 23:15:09 -0300

    Implementando Route Guard CanDeactivate na compra(123)

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -16,27 +16,15 @@ export class OrderComponent implements OnInit {
   numberPattern = /^[0-9]*$/;
   orderForm: FormGroup
 
+  // tslint:disable-next-line:no-inferrable-types
   delivery: number = 8;
+  orderId: string;
 
   paymentOptions: RadioOption[] = [
     { label: 'Dinheiro', value: 'MON' },
     { label: 'Cartão de Débito', value: 'DEB' },
     { label: 'Cartão de Refeição', value: 'REF' }
   ];
-
-  constructor(private orderService: OrderService, private router: Router, private formBuilder: FormBuilder) { }
-
-  ngOnInit() {
-    this.orderForm = this.formBuilder.group({
-      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]), //forma reduzida name:''
-      email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
-      emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
-      address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
-      number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
-      optionalAddress: this.formBuilder.control(''),
-      paymentOption: this.formBuilder.control('', [Validators.required])
-    }, { validator: OrderComponent.equalsTo })
-  }
 
   static equalsTo(group: AbstractControl): { [key: string]: boolean } {
     const email = group.get('email');
@@ -49,6 +37,22 @@ export class OrderComponent implements OnInit {
     }
     return undefined;
   }
+
+  constructor(private orderService: OrderService, private router: Router, private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]), // forma reduzida name:''
+      email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
+      optionalAddress: this.formBuilder.control(''),
+      paymentOption: this.formBuilder.control('', [Validators.required])
+    }, { validator: OrderComponent.equalsTo })
+  }
+
+
 
   itemsValue(): number {
     return this.orderService.itemsValue();
@@ -70,13 +74,18 @@ export class OrderComponent implements OnInit {
     this.orderService.remove(item);
   }
 
+  isOrderCompleted(): boolean {
+    return this.orderId !== undefined;
+  }
+
   checkOrder(order: Order) {
     order.orderItems = this.cartItems().map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
     console.log(order);
-    this.orderService.checkOrder(order).subscribe((orderId: string) => {
+    this.orderService.checkOrder(order).do((orderId: string) => this.orderId = orderId).subscribe((orderId: string) => {
       this.router.navigate(['/order-summary'])
       console.log(`Compra concluída: ${orderId}`);
       this.orderService.clear();
     });
   }
+
 }
